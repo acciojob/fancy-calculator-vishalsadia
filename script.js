9d62cb6a6748ec6a479f41754972a1fb554814ef
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-08 16:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// Fancy Calculator Program
+
+// Take input for the first variable and multiply it by 2
+const firstInput = parseInt(prompt("Enter the first number:"));
+const result = firstInput * 2;
+
+// Take input for the second variable
+const secondInput = parseInt(prompt("Enter the second number:"));
+
+// Add the second input to the result of the first input multiplied by 2
+const finalResult = result + secondInput;
+
+// Display the final result using alert() 
+alert("The final result is: " + finalResult);
